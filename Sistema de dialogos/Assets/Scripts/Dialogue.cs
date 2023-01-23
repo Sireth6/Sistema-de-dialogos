a9cfde03353186f37ca0cfaeb15696aa0d6a8b22
Sireth6 <fsvg06@gmail.com>
2023-01-22 20:46:48 -0600

    Creación de Script
    Se creó un script para el diálogo del NPC

---
+++ Sistema de dialogos/Assets/Scripts/Dialogue.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Dialogue : MonoBehaviour
+{
+    private bool isPlayerInRange;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            isPlayerInRange = true;
+            Debug.Log("Se puede iniciar un diálo");
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            isPlayerInRange = false;
+            Debug.Log("No se puede iniciar un dialogo");
+        }
+    }
+}
+  
