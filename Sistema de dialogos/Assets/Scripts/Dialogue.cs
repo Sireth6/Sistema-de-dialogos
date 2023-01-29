fdd2899b99b2a05272d0a75983e19cc30198ba91
Sireth6 <fsvg06@gmail.com>
2023-01-29 17:59:21 -0600

    Aparece Panel de texto
    El panel de texto ahora se muestra

--- Sistema de dialogos/Assets/Scripts/Dialogue.cs
+++ Sistema de dialogos/Assets/Scripts/Dialogue.cs
@@ -8,12 +8,56 @@ public class Dialogue : MonoBehaviour
     [SerializeField] private GameObject dialoguePanel;
     [SerializeField] private TMP_Text dialogueText;
     [SerializeField, TextArea(4, 6)] private string[] dialogueLines;
+
+    private float typingTime = 0.05f;
     private bool isPlayerInRange;
+    private bool didDialogueStart;
+    private int lineIndex;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(isPlayerInRange && Input.GetButtonDown("Submit"))
+        {
+            if(!didDialogueStart)
+            {
+               StarDialogue();
+            }
+        }
+    }
+    private void StarDialogue()
+    {
+        didDialogueStart = true;
+        dialoguePanel.SetActive(true);
+        dialogueMark.SetActive(false);
+        lineIndex = 0;
+        StartCoroutine(ShowLine());
+    }
+
+    private void NextDialogueLine()
+    {
+        lineIndex++;
+        if(lineIndex < dialogueLines.Length)
+        {
+            StarCoroutine(ShowLine());
+        }
+        else
+        {
+            didDialogueStart = false;
+            dialoguePanel.SetActive(false);
+            dialogueMark.SetActive(true);
+        }
+    }
+
+    private IEnumerator ShowLine()
+    {
+        dialogueText.text = string.Empty;
+
+        foreach(char ch in dialogueLines[lineIndex])
+        {
+            dialogueText.text += ch;
+            yield return new WaitForSeconds(typingTime);
+        }    
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
