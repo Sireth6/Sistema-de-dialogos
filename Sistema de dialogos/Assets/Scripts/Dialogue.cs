5e017f6193a65d000ca224692ca78448a5d74a68
Sireth6 <fsvg06@gmail.com>
2023-01-29 17:25:07 -0600

    Dialogos de texto
    Añadí los dialogos de texto

--- Sistema de dialogos/Assets/Scripts/Dialogue.cs
+++ Sistema de dialogos/Assets/Scripts/Dialogue.cs
@@ -1,9 +1,13 @@
 ﻿using System.Collections;
 using UnityEngine;
+using TMPro;
 
 public class Dialogue : MonoBehaviour
 {
-    [SerializeField] private GameObject dialogueMark; 
+    [SerializeField] private GameObject dialogueMark;
+    [SerializeField] private GameObject dialoguePanel;
+    [SerializeField] private TMP_Text dialogueText;
+    [SerializeField, TextArea(4, 6)] private string[] dialogueLines;
     private bool isPlayerInRange;
 
     // Update is called once per frame
